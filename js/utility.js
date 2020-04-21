d175b266e4026a9c8f34835ac1cbc85d28d2428a
Olufemi Daniel <oladotun.olufemi@yahoo.com>
2020-04-21 13:30:24 +0100

    minor discussed updates

--- js/utility.js
+++ js/utility.js
@@ -81,3 +81,7 @@ $(".custom-file-input").on("change", function () {
   let fileName = $(this).val().split("\\").pop();
   $(this).next(".custom-file-label").addClass("selected").html(fileName);
 });
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
