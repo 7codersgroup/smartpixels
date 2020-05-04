74facbc988456c38ad89f74caa35b2a4c4ab0be7
Samuel Oladipupo <psarmmiey@gmail.com>
2020-05-04 04:24:03 +0100

    follow-unfollow

---
+++ smartpixel_dev1/resources/views/layouts/ordinary_header.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>African stock images | Fricapix</title>
+    <meta name="Description" content="Smartpixels"/>
+    <meta name="_token" content="{{ csrf_token() }}">
+    <!--favicicon-->
+    <link rel="shortcut icon" href="{{asset ('img/favicon.jpg')}}"/>
+    <link rel="apple-touch-icon" href="{{asset ('img/favicon.jpg')}}"/>
+
+    <link rel="stylesheet" href="{{asset ('css/global.css')}}"/>
+    <link rel="stylesheet" href="{{asset ('css/custom-animation.css')}}"/>
+    <link rel="stylesheet" href="{{asset ('css/bootstrap.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset ('css/hamburgers.css')}}"/>
+
+    <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+</head>
+
+<body class="landing">
