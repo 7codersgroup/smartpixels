5de90c6402a5593ac7d44e8137746c12dc2b54eb
Samuel Oladipupo <psarmmiey@gmail.com>
2020-05-27 12:07:02 +0100

    Todo: reminder to inject google sign in when Design team is done order to accept login with google

--- smartpixel_dev1/resources/views/auth/login.blade.php
+++ smartpixel_dev1/resources/views/auth/login.blade.php
@@ -69,7 +69,7 @@
                                     <button type="submit" id="submit-button" class="btn btn-lg btn-block">
                                         Login
                                     </button>
-
+{{--TODO: add google sign in href="{{ url('auth/google') }}"--}}
 
                                 </form>
                             </div>
