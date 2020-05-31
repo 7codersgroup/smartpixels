2448dba3e6c6ae1e52c4d20f275e512c39b709f0
Samuel Oladipupo <psarmmiey@gmail.com>
2020-05-31 15:17:50 +0100

    Add and update banking details

---
+++ smartpixel_dev1/app/Http/Controllers/BankingController.php
@@ -0,0 +1,63 @@
+<?php
+	
+	namespace App\Http\Controllers;
+	
+	use App\{Banking, User};
+	use Illuminate\Support\Facades\Auth;
+	
+	class BankingController extends Controller
+	{
+		public function __construct ()
+		{
+			$this->middleware ('auth');
+		}
+		
+		public function index ()
+		{
+			$banking = Banking::where ('user_id', '=', Auth::id ())->first ();
+			return view ('payment-details', compact ('banking'));
+		}
+		
+		/**
+		 * @return \Illuminate\Http\RedirectResponse
+		 * @throws \Illuminate\Validation\ValidationException
+		 */
+		public function create ()
+		{
+			$user = User::find (Auth::id ());
+			$this->validate (request (), [
+				'bank_name' => 'required',
+				'account_no' => 'required',
+				'account_holder' => 'required'
+			]);
+			$bank = new Banking();
+			$bank->bank_name = \request ('bank_name');
+			$bank->account_no = \request ('account_no');
+			$bank->account_holder = \request ('account_holder');
+			$user->bank ()->save ($bank);
+			
+			return back ()->with ('success', 'Account details added successfully');
+		}
+		
+		public function update ()
+		{
+			$user = User::find (Auth::id ());
+			$this->validate (request (), [
+				'bank_name' => 'required',
+				'account_no' => 'required',
+				'account_holder' => 'required'
+			]);
+			
+			if (isset($user->bank)) {
+				$user->bank->bank_name = \request ('bank_name');
+				$user->bank->account_no = \request ('account_no');
+				$user->bank->account_holder = \request ('account_holder');
+				
+				$user->push ();
+			} else {
+				$this->create ();
+			}
+			
+			return back ()->with ('success', 'Account details updateds successfully');
+		}
+	}
